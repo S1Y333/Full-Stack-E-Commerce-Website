73f4dc2d4aae4995b89444e619b21ab7a2bcad0f
siyi zhang <siyizhang@siyis-MacBook-Pro.local>
2022-12-18 22:57:36 -0500

    First commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,18 @@
+package com.luv2code.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.luv2code.ecommerce.dao.ProductRepository;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+		
+
+	}
+
+
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Address.java
@@ -0,0 +1,50 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="street")
+	private String street;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="state")
+	private String state;
+	
+	@Column(name="country")
+	private String country;
+	
+	@Column(name="zip_code")
+	private String zipCode;
+	
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private Order order;
+
+}
